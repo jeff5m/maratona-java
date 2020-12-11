dd35ab8f5fc57b9f75d60598281ddb1c05a4683a
jeff5m <jeffmatheuspereira@gmail.com>
2020-12-11 19:38:00 -0300

    add declaration array rule

--- src/br/com/abc/intro/arrays/Arrays.java
+++ src/br/com/abc/intro/arrays/Arrays.java
@@ -4,7 +4,7 @@ public class Arrays {
     public static void main(String[] args) {
         // initialization methods
         int[] numbers = new int[5];
-        int[] numbers2 = {1, 2, 3, 4, 5};
+        int[] numbers2 = {1, 2, 3, 4, 5};  // only allowed in array declaration
         int[] numbers3 = new int[]{1, 2, 3, 4, 5};
 
         for (int i = 0; i < numbers2.length; i++) {
