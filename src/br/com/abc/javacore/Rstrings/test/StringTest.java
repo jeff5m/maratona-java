4381c4802896f763fe8490d573e14bedd7899fcc
jeff5m <jeffmatheuspereira@gmail.com>
2021-01-04 21:33:15 -0300

    Class 81 pt02 - strings useful methods

--- src/br/com/abc/javacore/Rstrings/test/StringTest.java
+++ src/br/com/abc/javacore/Rstrings/test/StringTest.java
@@ -1,5 +1,7 @@
 package br.com.abc.javacore.Rstrings.test;
 
+import java.util.Locale;
+
 public class StringTest {
     public static void main(String[] args) {
         String name = "Antonio";
@@ -9,5 +11,19 @@ public class StringTest {
 //        concatenated string
         name = name.concat(" Alberto");
         System.out.println(name);
+
+        String test = "Caio";
+        String test2 = "caio";
+        String test3 = "0123456789";
+        String test4 = "       0123456789        ";
+        System.out.println(test.charAt(3));
+        System.out.println(test.equals(test2));
+        System.out.println(test.equalsIgnoreCase(test2));
+        System.out.println(test.length());
+        System.out.println(test2.replace('c','p'));
+        System.out.println(test2.toUpperCase());
+        System.out.println(test3.substring(0,1)); // from the index, bring until position
+        System.out.println(test3.substring(0,2));
+        System.out.println(test4.trim()); // remove spaces at the beginning and at the end
     }
 }
