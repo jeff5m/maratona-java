efd66336120ff259e04ef676f488b022309ea7c6
jeff5m <jeffmatheuspereira@gmail.com>
2021-01-09 21:50:57 -0300

    Class 90 Regex pt02 - Metacharacter

--- src/br/com/abc/javacore/Tregex/test/RegularExpressions.java
+++ src/br/com/abc/javacore/Tregex/test/RegularExpressions.java
@@ -5,8 +5,15 @@ import java.util.regex.Pattern;
 
 public class RegularExpressions {
     public static void main(String[] args) {
-        String regex = "aba";
-        String text = "aba4234342baba";
+/*        \d - everything that is a digit
+          \D - everything that isn't a digit
+          \s - blank spaces
+          \S - blank spaces - everything that is in danger.
+          \w - letter and digits = everything that is a world character (a-z, A-Z, digits and _)
+          \W - letter and digits = everything that isn't a world character (a-z, A-Z, digits and _)
+ */
+        String regex = "\\w";
+        String text = "aba423434           2baba ";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(text);
         System.out.println("texto:  "+text);
