74232436dbeb95c846413bf1bc2cd4248e7d66bb
jeff5m <jeffmatheuspereira@gmail.com>
2021-01-09 22:18:17 -0300

    Class 91 Regex pt03 - User acess

--- src/br/com/abc/javacore/Tregex/test/RegularExpressions.java
+++ src/br/com/abc/javacore/Tregex/test/RegularExpressions.java
@@ -11,9 +11,12 @@ public class RegularExpressions {
           \S - blank spaces - everything that is in danger.
           \w - letter and digits = everything that is a world character (a-z, A-Z, digits and _)
           \W - letter and digits = everything that isn't a world character (a-z, A-Z, digits and _)
+          [] - select specific characters marked inside the square brackets
  */
-        String regex = "\\w";
-        String text = "aba423434           2baba ";
+//      hexadecimal number regex
+        int hexadecimalNumber = 0x100;
+        String regex = "0[xX][0-9a-fA-F]";
+        String text = "12 0x 0x 0xFFABC 0x10G 0x1";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(text);
         System.out.println("texto:  "+text);
@@ -21,7 +24,8 @@ public class RegularExpressions {
         System.out.println("expressao: "+matcher.pattern());
         System.out.println("posicoes encontradas");
         while (matcher.find()) {
-            System.out.print(matcher.start()+" ");
+            System.out.println(matcher.start()+" "+matcher.group());
         }
+
     }
 }
