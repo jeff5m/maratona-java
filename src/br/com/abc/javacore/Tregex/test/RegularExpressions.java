3712bb0f663a70fc26e1df08cac41946f784380f
jeff5m <jeffmatheuspereira@gmail.com>
2021-01-09 22:58:56 -0300

    Class 92 Regex pt04 - most used parameters

--- src/br/com/abc/javacore/Tregex/test/RegularExpressions.java
+++ src/br/com/abc/javacore/Tregex/test/RegularExpressions.java
@@ -12,11 +12,33 @@ public class RegularExpressions {
           \w - letter and digits = everything that is a world character (a-z, A-Z, digits and _)
           \W - letter and digits = everything that isn't a world character (a-z, A-Z, digits and _)
           [] - select specific characters marked inside the square brackets
- */
+          ? - zero or one
+          * - zero or more
+          + - one or more
+          {n,m} - select instances of character by how much they cab pay.\
+          () - group a expression together
+          | - add or comparison
+          $ - end line
+          . - joker, can replace anything
+*/
+
 //      hexadecimal number regex
-        int hexadecimalNumber = 0x100;
-        String regex = "0[xX][0-9a-fA-F]";
-        String text = "12 0x 0x 0xFFABC 0x10G 0x1";
+//        int hexadecimalNumber = 0x1;
+//        String regex = "0[xX]([0-9a-fA-F])+(\\s|$)";
+//        String text = "12 0x 0x 0x01FFABC 0x10G 0x1";
+        String regex = "([\\w\\._-])+@([a-zA-Z])+(\\.([a-zA-Z])+)+";
+        String text = "fulano@gmail.com, cicla-no@hotmail.com, 12345@email, asdf@gmail.com, test@outlook.com.br";
+        System.out.println("Email valido? "+"$#@validationtest@mail".matches(regex));
+
+//      DATES
+//        String regexDate = "\\d\\d/\\d\\d/\\d\\d\\d\\d";
+        String regexDate = "\\d{2}/\\d{2}/\\d{2,4}";
+        String textDate = "05/10/2010 05/05/2015 1/1/01 01/05/95";
+
+        printRegex(textDate, regexDate);
+    }
+
+    private static void printRegex(String text, String regex) {
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(text);
         System.out.println("texto:  "+text);
@@ -26,6 +48,5 @@ public class RegularExpressions {
         while (matcher.find()) {
             System.out.println(matcher.start()+" "+matcher.group());
         }
-
     }
 }
