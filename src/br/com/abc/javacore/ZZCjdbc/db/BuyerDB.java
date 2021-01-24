2a81c15753b33fe6b43fa0fbf3171c4edfbb2687
jeff5m <jeffmatheuspereira@gmail.com>
2021-01-24 20:44:30 -0300

    Class 144: update and delete data from DB

--- src/br/com/abc/javacore/ZZCjdbc/db/BuyerDB.java
+++ src/br/com/abc/javacore/ZZCjdbc/db/BuyerDB.java
@@ -8,15 +8,52 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class BuyerDB {
-    public void save(Buyer buyer) {
+    public static void save(Buyer buyer) {
         String sql = "INSERT INTO `maratona_java`.`buyer` (`cpf`, `name`) VALUES ('" + buyer.getCpf() + "', '" + buyer.getName() + "');";
         Connection conn = ConnectionFactory.getConnection();
         try {
             Statement stat = conn.createStatement();
 //            if the query update state of the DB, use .executeUpdate();
 //            if we are retrieving data from the DB, .executeQuery();
-            System.out.println(stat.executeUpdate(sql));
+            stat.executeUpdate(sql);
             ConnectionFactory.close(conn, stat);
+            System.out.println("Registro inserido com sucesso!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void delete(Buyer buyer) {
+        if (buyer == null || buyer.getId() == null) {
+            System.out.println("Ops! Não foi possível excluir o registro!");
+            return;
+        }
+
+        String sql = "DELETE FROM `maratona_java`.`buyer` WHERE (`id` = '" + buyer.getId() + "');";
+        Connection conn = ConnectionFactory.getConnection();
+        try {
+            Statement stat = conn.createStatement();
+            stat.executeUpdate(sql);
+            ConnectionFactory.close(conn, stat);
+            System.out.println("Registro excluido com sucesso!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void update(Buyer buyer) {
+        if (buyer == null || buyer.getId() == null) {
+            System.out.println("Ops! Não foi possível atualizar o registro!");
+            return;
+        }
+
+        String sql = "UPDATE `maratona_java`.`buyer` SET `cpf` = '" + buyer.getCpf() + "', `name` = '" + buyer.getName() + "' WHERE (`id` = '" + buyer.getId() + "');";
+        Connection conn = ConnectionFactory.getConnection();
+        try {
+            Statement stat = conn.createStatement();
+            stat.executeUpdate(sql);
+            ConnectionFactory.close(conn, stat);
+            System.out.println("Regisitro atualizado com sucesso!");
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- src/br/com/abc/javacore/ZZCjdbc/test/ConnectionTest.java
+++ src/br/com/abc/javacore/ZZCjdbc/test/ConnectionTest.java
@@ -5,8 +5,24 @@ import br.com.abc.javacore.ZZCjdbc.db.BuyerDB;
 
 public class ConnectionTest {
     public static void main(String[] args) {
+//        insert();
+//        delete();
+        update();
+    }
+
+    public static void insert() {
         Buyer buyer = new Buyer("111.222.333-44", "Maria");
-        BuyerDB buyerDB = new BuyerDB();
-        buyerDB.save(buyer);
+        BuyerDB.save(buyer);
+    }
+
+    public static void delete() {
+        Buyer buyer = new Buyer();
+        buyer.setId(2);
+        BuyerDB.delete(buyer);
+    }
+
+    public static void update() {
+        Buyer buyer = new Buyer(1,"000.000.000-11","MARIA");
+        BuyerDB.update(buyer);
     }
 }
