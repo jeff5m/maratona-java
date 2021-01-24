81bbefd8373e6ef4ed6cb43716d3676b6345e587
jeff5m <jeffmatheuspereira@gmail.com>
2021-01-24 14:47:18 -0300

    update: use env variables instead of .props file

--- src/br/com/abc/javacore/ZZCjdbc/conn/ConnectionFactory.java
+++ src/br/com/abc/javacore/ZZCjdbc/conn/ConnectionFactory.java
@@ -10,25 +10,19 @@ import java.util.Properties;
 
 public class ConnectionFactory {
 //    java.sql main interfaces and classes:
-//      DriverManager (class that search for the specific connector for the DB and return a Connection),
+//      DriverManager (class that search for the specific connector for the DB and returns a Connection),
 //      Connection (get connection with DB),
 //      Statement (responsible for operation that will change Db state),
 //      ResultSet (responsible for query results)
     public Connection getConnection() {
-
-        try (FileReader reader = new FileReader("config.properties")) {
-            Properties properties = new Properties();
-            properties.load(reader);
-
-            String url = properties.getProperty("url");
-            String username = properties.getProperty("username");
-            String password = properties.getProperty("password");
-//            in old days, we need to explicit load the Driver.
-//            Class.forName("com.mysql.jdbc.Driver");
+        try {
+            String url = System.getenv("MYSQL_URL");
+            String username = System.getenv("MYSQL_USERNAME");
+            String password = System.getenv("MYSQL_PASSWORD");
             Connection connection = DriverManager.getConnection(url, username, password);
             System.out.println(connection);
             return connection;
-        } catch (SQLException | IOException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
