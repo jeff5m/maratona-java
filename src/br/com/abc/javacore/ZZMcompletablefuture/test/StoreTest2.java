ed357ca32cf5f02b64bbec42e36184adde806113
jeff5m <jeffmatheuspereira@gmail.com>
2021-02-14 21:15:56 -0300

    199: Java 8: completableFuture custom Executor

--- src/br/com/abc/javacore/ZZMcompletablefuture/test/StoreTest2.java
+++ src/br/com/abc/javacore/ZZMcompletablefuture/test/StoreTest2.java
@@ -4,6 +4,8 @@ import br.com.abc.javacore.ZZMcompletablefuture.classes.Store;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 import static java.util.Arrays.asList;
@@ -52,6 +54,68 @@ public class StoreTest2 {
                 new Store("bestBuy"),
                 new Store("walmart"),
                 new Store("amazon "),
+                new Store("amazon "),
+                new Store("bestBuy"),
+                new Store("walmart"),
+                new Store("amazon "),
+                new Store("bestBuy"),
+                new Store("walmart"),
+                new Store("amazon "),
+                new Store("bestBuy"),
+                new Store("walmart"),
+                new Store("amazon "),
+                new Store("bestBuy"),
+                new Store("walmart"),
+                new Store("amazon "),
+                new Store("bestBuy"),
+                new Store("walmart"),
+                new Store("amazon "),
+                new Store("bestBuy"),
+                new Store("walmart"),
+                new Store("amazon "),
+                new Store("bestBuy"),
+                new Store("walmart"),
+                new Store("amazon "),
+                new Store("bestBuy"),
+                new Store("walmart"),
+                new Store("amazon "),
+                new Store("bestBuy"),
+                new Store("walmart"),
+                new Store("amazon "),
+                new Store("bestBuy"),
+                new Store("walmart"),
+                new Store("amazon "),
+                new Store("amazon "),
+                new Store("bestBuy"),
+                new Store("walmart"),
+                new Store("amazon "),
+                new Store("bestBuy"),
+                new Store("walmart"),
+                new Store("amazon "),
+                new Store("bestBuy"),
+                new Store("walmart"),
+                new Store("amazon "),
+                new Store("bestBuy"),
+                new Store("walmart"),
+                new Store("amazon "),
+                new Store("bestBuy"),
+                new Store("walmart"),
+                new Store("amazon "),
+                new Store("bestBuy"),
+                new Store("walmart"),
+                new Store("amazon "),
+                new Store("bestBuy"),
+                new Store("walmart"),
+                new Store("amazon "),
+                new Store("bestBuy"),
+                new Store("walmart"),
+                new Store("amazon "),
+                new Store("bestBuy"),
+                new Store("walmart"),
+                new Store("amazon "),
+                new Store("bestBuy"),
+                new Store("walmart"),
+                new Store("amazon "),
                 new Store("bestBuy"),
                 new Store("walmart"),
                 new Store("north"));
@@ -62,7 +126,23 @@ public class StoreTest2 {
 //        async CompletableFuture
         findPricesCompletableFuture(stores);
 //        in performance matters, parallel streams and completableFuture are very similar.
-//        The advantage of use CompletableFuture is that he allows to use a Executor
+//        The advantage of use CompletableFuture is that he allows to use a custom Executor and this improve performance
+//        To use an Executor, we need to tell to our program the number of threads for our thread poll. To calculate
+//        this number, Brian Goetz suggest use the formula:
+//        Nthreads = Ncpu * Ucpu * (1 + W/C)
+//        Ncpu = number of cores available
+//        Ucpu = percentage of time utilization of cpu (0-1, where 1 its 100%)
+//        W/C = Wait time and Compute time
+//        Exemple:
+//            Nthreads = 4 * 1 * (1+99) = 400
+        final Executor executor = Executors.newFixedThreadPool(Math.min(stores.size(), 100), r -> {
+            Thread t = new Thread(r);
+            t.setDaemon(true); // this threads can be finished when the others non daemon thread are finished
+            return t;
+        });
+        findPricesCompletableFuture(stores, executor);
+//        when to use streams -> computing data without I/O, without wait data from any source
+//        when to use CompletableFuture -> computing data with I/O, waiting for data from any source
     }
 
     private static List<String> findPrices(List<Store> stores) {
@@ -112,6 +192,24 @@ public class StoreTest2 {
                 .map(store -> CompletableFuture.supplyAsync(
                         () -> String.format("%s o preco eh: %.2f", store.getName(), store.getPrice())
                 ))
+                .collect(Collectors.toList());
+        System.out.println("Tempo de invocacao eh de " + (System.currentTimeMillis() - start) + " ms");
+
+        List<String> collect = futurePrice.stream().map(CompletableFuture::join).collect(Collectors.toList());
+        System.out.println("Tempo de total " + (System.currentTimeMillis() - start) + " ms");
+        System.out.println(collect);
+        return collect;
+    }
+
+    private static List<String> findPricesCompletableFuture(List<Store> stores, Executor executor) {
+//    returns a string with stores name and price
+        System.out.println("Completable future");
+        long start = System.currentTimeMillis();
+        List<CompletableFuture<String>> futurePrice = stores.stream()
+                .map(store -> CompletableFuture
+                        .supplyAsync(
+                                () -> String.format("%s o preco eh: %.2f", store.getName(), store.getPrice()), executor
+                        ))
                 .collect(Collectors.toList());
         System.out.println("Tempo de invocacao eh de " + (System.currentTimeMillis() - start) + " ms");
 
